7115b525dee9ea2edc0a36b4788880dd584636e2
jtmccr1 <mccronejt@gmail.com>
2024-04-30 15:08:38 -0500

    rectangular stub

--- packages/core/src/Layouts/functional/rectangularLayout.test.ts
+++ packages/core/src/Layouts/functional/rectangularLayout.test.ts
@@ -5,19 +5,20 @@ import { rectangularLayout } from "./rectangularLayout";
 describe("Test rectangular layout",()=>{
     it('check x and y on root', function() {
         const tree = ImmutableTree.fromNewick("((a:1,b:1):1,c:1);");
-        const root = rectangularLayout(tree, tree.getRoot());
+        const layout = rectangularLayout();
+        const root = layout(tree, tree.getRoot());
         expect(root.x).toBeCloseTo(0);
         expect(root.y).toBeCloseTo(1.25); 
 
         // //a
         const a = tree.getNodeByTaxon("a")!;
-        const aV = rectangularLayout(tree,a)
+        const aV = layout(tree,a)
         expect(aV.x).toBeCloseTo(2);
         expect(aV.y).toBeCloseTo(0); 
 
         // //c
         const c = tree.getNodeByTaxon("c")!;
-        const cV = rectangularLayout(tree,c)
+        const cV = layout(tree,c)
         expect(cV.x).toBeCloseTo(1);
         expect(cV.y).toBeCloseTo(2); 
     });
@@ -27,14 +28,15 @@ describe("Test rectangular layout",()=>{
         
         const tree = ImmutableTree.fromNewick("((a:1,b:1):1,c:1);")
                                     .treeSubscribeCallback((tree,node)=>cache.handleUpdate(tree,node));
-        const root = rectangularLayout(tree, tree.getRoot(),cache);
+        const layout = rectangularLayout(cache);
+        const root = layout(tree, tree.getRoot());
         const c = tree.getNodeByTaxon("c")!;
-        const cV = rectangularLayout(tree,c)
+        const cV = layout(tree,c)
         const tree2 = tree.setDivergence(tree.getNodeByTaxon('a')!,3.0)
 
-        const root2 = rectangularLayout(tree2,tree2.getRoot(),cache);
+        const root2 = layout(tree2,tree2.getRoot());
         const c2 = tree2.getNodeByTaxon("c")!;
-        const cV2 = rectangularLayout(tree,c2,cache)
+        const cV2 = layout(tree,c2)
         expect(c2).toStrictEqual(c)
         expect(cV2).toStrictEqual(cV)
         // check a parent should 
--- packages/core/src/Layouts/functional/rectangularLayout.ts
+++ packages/core/src/Layouts/functional/rectangularLayout.ts
@@ -10,31 +10,31 @@ type Vertex = {
 }
 
 
-export function rectangularLayout(tree:Tree,node:NodeRef,traversal:PreOrderTraversalCache = new PreOrderTraversalCache()):Vertex{
-    if( traversal.cache.has(node))return traversal.cache.get(node)!
-        const x = tree.getDivergence(node)!;
-        const predecessor = traversal.getPrevious(tree,node)
-        if(tree.isExternal(node)){
-            if(predecessor){
-                const currentY = rectangularLayout(tree,predecessor!,traversal).maxY; // here recursion happens 
-                const vertex = {x,y:currentY+1,maxY:currentY+1,maxX:x}
-                traversal.cache.set(node,vertex)
-            }else{
-                //at first node
-                const vertex = {x,y:0,maxY:0,maxX:x}
+export function rectangularLayout (traversal:PreOrderTraversalCache = new PreOrderTraversalCache()){
+    return function layout(tree:Tree,node:NodeRef):Vertex{
+        if( traversal.cache.has(node))return traversal.cache.get(node)!
+            const x = tree.getDivergence(node)!;
+            const predecessor = traversal.getPrevious(tree,node)
+            if(tree.isExternal(node)){
+                if(predecessor){
+                    const currentY = layout(tree,predecessor!).maxY; // here recursion happens 
+                    const vertex = {x,y:currentY+1,maxY:currentY+1,maxX:x}
                     traversal.cache.set(node,vertex)
-               
-                            }
-        }else{
-            const kidPositions = tree.getChildren(node).map(child=>rectangularLayout(tree,child,traversal))
-            const y= mean(kidPositions,d=>d.y)!;
-            const maxY = max(kidPositions,d=>d.maxY)!;
-            const maxX = max(kidPositions,d=>d.maxX)!;
-            const vertex = {x,y,maxY,maxX}
-            traversal.cache.set(node,vertex)
+                }else{
+                    //at first node
+                    const vertex = {x,y:0,maxY:0,maxX:x}
+                        traversal.cache.set(node,vertex)
+                }
+            }else{
+                const kidPositions = tree.getChildren(node).map(child=>layout(tree,child))
+                const y= mean(kidPositions,d=>d.y)!;
+                const maxY = max(kidPositions,d=>d.maxY)!;
+                const maxX = max(kidPositions,d=>d.maxX)!;
+                const vertex = {x,y,maxY,maxX}
+                traversal.cache.set(node,vertex)
 
+            }
+            return traversal.cache.get(node);
         }
-        return traversal.cache.get(node);
-    }
 
-
+}
