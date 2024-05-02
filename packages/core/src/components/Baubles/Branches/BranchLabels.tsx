91d809cc3adac21b8a62952d8ee904777e8950ba
jtmccr1 <mccronejt@gmail.com>
2024-05-01 23:21:17 -0500

    branch labels working with polar layout

--- packages/core/src/components/Baubles/Branches/BranchLabels.tsx
+++ packages/core/src/components/Baubles/Branches/BranchLabels.tsx
@@ -5,26 +5,60 @@ import Label from "../Nodes/Shapes/Label";
 import { NodeRef } from "../../../Evo/Tree/Tree.types";
 import { useFigtreeStore, useVertex } from "../../../store/store";
 import { preOrderIterator } from "../../../Evo/Tree";
+import { textSafeDegrees } from "../../../store/polarScale";
 
 
 
 export default function BranchLabels(props:any){
-    const {filter=(n:NodeRef)=>true,aligned=false,...rest} = props;
+    const {filter=(n:NodeRef)=>true,aligned=false,gap=6,...rest} = props;
     const shapeProps = useAttributeMappers(props);
 
     const tree = useFigtreeStore(state=>state.tree);  
     const scale = useFigtreeStore(state=>state.scale);
 
+
+
+
 return (
     <g className={"node-label-layer"}>
         {[...preOrderIterator(tree)].filter(node=>filter(node) && !tree.isRoot(node)).map((node) => { 
-                const v = scale(useVertex(node));
-                const vP = scale(useVertex(tree.getParent(node)!));
-                return <Label key={node.number} {...rest}  node={node}  alignmentBaseline={"bottom"} textAnchor={"middle"} rotation={0} x = {(v.x+vP.x)/2}  y={v.y-6} {...shapeProps(node)}/> 
+                const v = useVertex(node);
+                const parentVertex = useVertex(tree.getParent(node)!);
+                const scaledV = scale(useVertex(node));
+                const scaledpV = scale(parentVertex);
+                const layoutClass = v.layoutClass;
+
+                const rotation = layoutClass==="Polar"?textSafeDegrees(scaledV.theta!):0;
+                const step = scale({x:parentVertex.x,y:v.y})
+                const {dx,dy} = layoutClass==="Polar"? getPolarBranchDs(scaledV.theta!,gap):{dx:0,dy:-1*gap};
+                const x = (layoutClass==="Polar"? (scaledV.x+step.x)/2 : (scaledV.x+scaledpV.x)/2 )+dx;
+                const y = (layoutClass==="Polar"? (scaledV.y+step.y)/2  : scaledV.y )+dy;
+
+                return <Label key={node.number} {...rest}  node={node}  alignmentBaseline={"bottom"} textAnchor={"middle"} rotation={rotation} x = {x}  y={y} {...shapeProps(node)}/> 
                 // const element = <ShapeComponent key={v.id} {...rest}  {...shapeProps(v)}   vertex={v}  x={scales.x(v.x)} y={scales.y(v.y)}/> 
             })
         
         }
     </g>
 )
+}
+
+
+function getPolarBranchDs(theta:number,gap:number){
+                //branch lable dx dy;
+                let branchDx,branchDy;
+                if(theta>0 && theta<Math.PI/2){//good
+                    branchDx = Math.sin((Math.PI/2) -theta)*gap;
+                    branchDy = -Math.cos((Math.PI/2) -theta)*gap;
+                }else if(theta>Math.PI/2 && theta<Math.PI){ //good
+                    branchDx = -Math.cos((Math.PI/2) - (Math.PI-theta))*gap;
+                    branchDy = -Math.sin((Math.PI/2) - (Math.PI-theta))*gap;
+                }else if (theta>Math.PI && theta<3*Math.PI/2){ // good
+                    branchDx = Math.cos((Math.PI/2) - (theta-Math.PI))*gap;
+                    branchDy = -Math.sin((Math.PI/2) - (theta-Math.PI))*gap;
+                }else{
+                    branchDx = -Math.cos((Math.PI/2) - (2*Math.PI-theta))*gap;
+                    branchDy = -Math.sin((Math.PI/2) - (2*Math.PI-theta))*gap;
+                }
+                return {dx:branchDx,dy:branchDy};
 }
